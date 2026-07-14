f30205812426b47179c8ef217d57835666e078bd
amjadramzi1998-a11y <amjadramzi1998@gmail.com>
2026-07-14 15:19:37 +0300

    Fix home page cache

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import Header from "../components/Header";
 import SearchBar from "../components/SearchBar";
 import CategoryCard from "../components/CategoryCard";
